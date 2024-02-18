34382876d26aaeb227c15e5f41bc9a912ab51d37
Nasreddine Ouldei Tebina <tebina1@live.fr>
2024-02-18 17:48:42 +0100

    added blif example and ST technology mapping example

--- mappings.hpp
+++ mappings.hpp
@@ -2,13 +2,15 @@
 
 #pragma once
 
-#include <unordered_map>
 #include <string>
+#include <unordered_map>
 
 const std::unordered_map<std::string, std::string> keywordMap = {
-        {"AND2X1", "MY_AND2X1"},
-        {"OR2X1", "MY_OR2X1"},
-        {"NAND2X1", "MY_NAND2X1"}
-        // Add more mappings as needed
+    {"AND2X1", "HS65_GS_AND2x4"},
+    {"OR2X1", "HS65_GS_OR2x4"},
+    {"Y=", "Z="},
+    {"NAND2X1", "HS65_GS_NAND2x4"},
+    {"NOR2X1", "HS65_GS_NOR2x4"},
+    {"INVX1", "HS65_GS_IVX9"}
+    // Add more mappings as needed
 };
-
